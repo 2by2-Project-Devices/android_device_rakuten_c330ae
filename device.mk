069a81799c5bc8bfbe5c607621933d6a9b2e74c5
kailua <kailua@project2by2.jp>
2026-05-02 20:42:09 +0900

    c330ae: Build camera stack

--- device.mk
+++ device.mk
@@ -141,7 +141,8 @@ TARGET_BOOTANIMATION_HALF_RES := true
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service
+    android.hardware.camera.provider@2.4-service \
+    camera.msm8937
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor
