ce30d7651acf8257500439e05fc47fca3c9f116c
Michael Bestas <mkbestas@lineageos.org>
2026-05-28 20:09:58 +0900

    c330ae: Convert rootdir to init

--- device.mk
+++ device.mk
@@ -215,6 +215,21 @@ PRODUCT_PACKAGES += \
     android.hardware.health-service.qti \
     android.hardware.health-service.qti_recovery
 
+# Init
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    fstab.qcom.ramdisk \
+    fstab.zram \
+    init.msm.usb.configfs.rc \
+    init.qcom.post_boot.sh \
+    init.qcom.rc \
+    init.qcom.sh \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh \
+    init.recovery.qcom.rc \
+    init.target.rc \
+    ueventd.qcom.rc
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/ft5435_ts.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/ft5435_ts.kl \
@@ -324,21 +339,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
-
-# Ramdisk
-PRODUCT_PACKAGES += \
-    fstab.qcom \
-    fstab.qcom.ramdisk \
-    fstab.zram \
-    init.msm.usb.configfs.rc \
-    init.qcom.post_boot.sh \
-    init.qcom.rc \
-    init.qcom.sh \
-    init.qcom.usb.rc \
-    init.qcom.usb.sh \
-    init.recovery.qcom.rc \
-    init.target.rc \
-    ueventd.qcom.rc
 
 # Regulatory info
 PRODUCT_PACKAGES += \
