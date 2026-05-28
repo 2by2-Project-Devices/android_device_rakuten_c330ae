9a6db597a4673d149b0fc278813c954bc9327ed5
kailua <kailua@project2by2.jp>
2026-05-28 20:15:03 +0900

    c330ae: configs: Drop duplicated thermal-engine.conf
    * Already in proprietary-files

--- device.mk
+++ device.mk
@@ -412,9 +412,6 @@ WITH_DEXPREOPT_DEBUG_INFO := false
 USE_DEX2OAT_DEBUG := false
 
 # Thermal
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
-
 PRODUCT_PACKAGES += \
     android.hardware.thermal-service.qti.rakuten_c330ae
 
