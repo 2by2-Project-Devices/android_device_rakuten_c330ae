8fa441338c48ee3f97375830ea23c81713a816cf
Aditya Eka Pranata <aepranata@gmail.com>
2026-06-02 07:34:55 +0700

    c330ae: Up reserved size on product partition to 800MB
    Change-Id: I54a0fbf6bdb93c98a3e19f88170c41f83bd29496

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -154,6 +154,8 @@ $(foreach p, $(call to-upper, $(SSI_PARTITIONS)), \
 $(foreach p, $(call to-upper, $(TREBLE_PARTITIONS)), \
     $(eval BOARD_$(p)IMAGE_PARTITION_RESERVED_SIZE := 41943040)) # 40 MB
 
+BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE := 838860800
+
 # Peripheral manager
 TARGET_PER_MGR_ENABLED := true
 
