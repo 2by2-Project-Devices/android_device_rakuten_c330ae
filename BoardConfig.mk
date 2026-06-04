93128dfa4bc8490c29900c4a53c9a95842cb976b
Michael Bestas <mkbestas@lineageos.org>
2026-06-04 21:00:32 +0900

    c330ae: Migrate QTI power HAL to soong_config_set
    Change-Id: Ib80c15ee523130011147d378f665592dae501ec2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -129,7 +129,6 @@ BOARD_VENDORIMAGE_PARTITION_SIZE := 1073741824
 TARGET_PER_MGR_ENABLED := true
 
 # Power
-TARGET_TAP_TO_WAKE_NODE := "/sys/bus/i2c/drivers/fts_ts/3-0038/fts_gesture_mode"
 TARGET_USES_INTERACTION_BOOST := true
 
 # Qualcomm
--- device.mk
+++ device.mk
@@ -324,6 +324,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint/system/powerhint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/powerhint.xml \
     $(LOCAL_PATH)/configs/powerhint/vendor/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+$(call soong_config_set,qtipower,tap_to_wake_node,/sys/bus/i2c/drivers/fts_ts/3-0038/fts_gesture_mode)
+
 # Public libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
