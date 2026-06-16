c2bc8b96c69d6c67bd08008c6b87c1f1e7a80235
kailua <kailua@project2by2.jp>
2026-06-16 18:13:41 +0900

    c330ae: Switch to EROFS when gapps is included

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -129,8 +129,14 @@ SSI_PARTITIONS := product system system_ext
 TREBLE_PARTITIONS := odm vendor
 ALL_PARTITIONS := $(SSI_PARTITIONS) $(TREBLE_PARTITIONS)
 
+ifneq ($(WITH_GMS),true)
+TARGET_RO_FILE_SYSTEM_TYPE := ext4
+else
+TARGET_RO_FILE_SYSTEM_TYPE := erofs
+endif
+
 $(foreach p, $(call to-upper, $(ALL_PARTITIONS)), \
-    $(eval BOARD_$(p)IMAGE_FILE_SYSTEM_TYPE := ext4) \
+    $(eval BOARD_$(p)IMAGE_FILE_SYSTEM_TYPE := $(TARGET_RO_FILE_SYSTEM_TYPE)) \
     $(eval TARGET_COPY_OUT_$(p) := $(call to-lower, $(p))))
 
 BOARD_SUPER_PARTITION_BLOCK_DEVICES := system vendor
