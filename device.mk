11cc554bb66f8ba2cfc28b1dbeeb7a9dc3948dfe
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2026-06-17 18:54:09 +0900

    c330ae: Drop SoundTrigger
    * Never ensured it's working in before
    * ST 2.1 interface is deprecated on vintf target-level 6,
      which is the max that our ST HAL provides
    * Let's see if there's any luck of bringing up OSS ST HAL?
    
    Change-Id: I189e0dca38f74be0343c5b345481f3bb7c95bc65

--- device.mk
+++ device.mk
@@ -93,7 +93,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl:32 \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.service \
-    android.hardware.soundtrigger@2.3-impl:32 \
     audio.bluetooth.default \
     audio.primary.msm8937:32 \
     audio.r_submix.default \
